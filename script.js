ecfccf299d1c5382d09f7a4b8ed5151a2c08a189
Prashant Pal <palprashant465@gmail.com>
2025-03-18 17:50:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+
+document.getElementById('voteForm').addEventListener('submit',(event)=>{
+	event.preventDefault();
+
+	const age = document.getElementById('age').value.trim();
+	const name = document.getElementById('name').value.trim();
+
+	if(!age || !name){
+		alert("Please enter valid details");
+	}
+
+	const ageNum = Number(age);
+    const VoteEligibility = new Promise((res,rej)=>{
+		setTimeout(()=>{
+			if(ageNum>18){
+				res(`Welcome, .${name} You can vote.`)
+			}
+			else{
+				rej(`Oh sorry . You aren't old enough.`)
+			}
+		},4000)
+	});
+
+	VoteEligibility.then((message)=>{
+		alert(message);
+	}).catch((err)=>{
+		alert(err)
+	})
+
+
+	
+	
+})
